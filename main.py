07103d0449beb0b5ef382fba2f58f59b6391b0cc
escartigui <escarlethmayali@gmail.com>
2025-07-23 18:32:17 -0600

    escarleth

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hola")
+clientes = {}
+def menu():
+    print("Bienvenido al cliente del usuario")
+    print("1.Ingreso de cliente")
+    print("2.Mostrar")
+    print("3.Salir")
+    op = int(input("Ingrese su opción"))
+
+    if op == 1:
