e9d5617086deb31e8dcbf36faeb31bc7a8a7ada0
escartigui <escarlethmayali@gmail.com>
2025-07-23 19:12:54 -0600

    escarleth

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 clientes = {}
 def menu():
   while True:
-    print("Bienvenido al cliente del usuario")
-    print("1.Ingreso de cliente")
-    print("2.Mostrar")
-    print("3.Salir")
-    op = int(input("Ingrese su opción"))
+       print("Bienvenido al menu del usuario")
+       print("1.Ingreso de cliente")
+       print("2.Mostrar")
+       print("3.Salir")
+       op = int(input("Ingrese su opción: "))
 
-    if op == "1":
+       if op == "1":
         cantidad = int(input("Ingrese la cantidad de clientes: "))
         for i in range(cantidad):
             print(f"Cliente:{i+1}")
             while True:
-                print("ingrese codigo del cliente: ")
-                codigo = input()
+                codigo = input("ingrese codigo del cliente: ")
                 if codigo in clientes:
                     print(f"El cliente ya existe")
                 else:
-                 clientes[codigo] = {}
+                    clientes[codigo] = {}
+                    break
+
             clientes[codigo]["nombre"] = input("Ingrese el nombre del cliente: ")
             clientes[codigo]["viajes"] = {}
             cantiviaje = int(input("Ingrese la cantidad de viajes: "))
@@ -27,8 +28,9 @@ def menu():
                 clientes[codigo]["viajes"][nombredestino] = {
                     "Destino" : nombredestino,
                 }
-    if op == "2":
-        print("\nListado")
+
+        if op == "2":
+         print("\nListado")
         for codigo, datos in clientes.items():
             print(f"\nCodigo: { codigo }")
             print(f"Nombre: { datos['nombre'] }")
